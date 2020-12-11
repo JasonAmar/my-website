d57017587e8fc80939aa6e5c5dc69850e60af779
jasonamar <jasona@hexaware.com>
2020-12-10 22:17:54 -0500

    Edited header.js to be flexbox

--- components/header.js
+++ components/header.js
@@ -1,53 +1,62 @@
+const headerTemplate = document.createElement('template');
+headerTemplate.innerHTML = `
+    <style>
+    nav {
+        display: flex;
+        background-color: #333;
+        overflow: hidden;
+    }
+
+    /* Style the links inside the navigation bar */
+    a {
+        color: #f2f2f2;
+        text-align: center;
+        padding: 1em;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    ul {
+        margin: 0;
+        padding: 1em;
+        padding-left: 0;
+    }
+
+    li{
+        list-style-type: none;
+        display:inline;
+    }
+
+    /* Change the color of links on hover */
+    a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+
+    /* Add a color to the active/current link */
+    a.active {
+        background-color: #4CAF50;
+        color: white;
+    }
+    </style>
+
+    <nav>
+        <ul>
+            <li><a class="active" href="index.html">Home</a></li>
+            <li><a href="projects.html">Projects</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="about.html">About</a></li>
+        </ul>
+    </nav>`;
+
 class Header extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        this.innerHTML = `
-            <style>
-            .topnav {
-                background-color: #333;
-                overflow: hidden;
-            }
-
-            /* Style the links inside the navigation bar */
-            .topnav a {
-                float: left;
-                color: #f2f2f2;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-                font-size: 17px;
-            }
-
-            .topnav ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-            }
-
-            /* Change the color of links on hover */
-            .topnav a:hover {
-                background-color: #ddd;
-                color: black;
-            }
-
-            /* Add a color to the active/current link */
-            .topnav a.active {
-                background-color: #4CAF50;
-                color: white;
-            }  
-            </style>    
-        
-            <nav class="topnav">
-                <ul>
-                    <li><a class="active" href="index.html">Home</a></li>
-                    <li><a href="projects.html">Projects</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                    <li><a href="about.html">About</a></li>
-                </ul>
-            </nav>`;
+        const shadowRoot = this.attachShadow({mode:'closed'});
+        shadowRoot.appendChild(headerTemplate.content);
     }
 }
 
