f12bae358d19c1d1fa9288ec886525b16293453e
jasonamar <jasona@hexaware.com>
2020-12-14 21:42:15 -0500

    Added CSS to space out header and footer component

--- components/header.js
+++ components/header.js
@@ -40,14 +40,16 @@ headerTemplate.innerHTML = `
     }
     </style>
 
-    <nav>
-        <ul>
-            <li><a class="active" href="index.html">Home</a></li>
-            <li><a href="projects.html">Projects</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="about.html">About</a></li>
-        </ul>
-    </nav>`;
+    <header>
+        <nav>
+            <ul>
+                <li><a class="active" href="index.html">Home</a></li>
+                <li><a href="projects.html">Projects</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="about.html">About</a></li>
+            </ul>
+        </nav>
+    <header>`;
 
 class Header extends HTMLElement {
     constructor() {
